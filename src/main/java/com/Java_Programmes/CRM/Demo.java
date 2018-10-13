873709a4e34796cff7e8fbcd46c1c2365a8e7dda
YMKRAJU <kanta2121@gmail.com>
2018-10-13 11:37:45 +0530

    java class is renamed to Demo

--- src/main/java/com/Java_Programmes/CRM/Demo.java
+++ src/main/java/com/Java_Programmes/CRM/Demo.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class App 
+public class Demo 
 {
     public static void main( String[] args )
     {
